fbc6e920c495a10b35709ce9b28218e10011b173
Ros Haitovich <rhaitovich@salesforce.com>
2016-01-28 06:39:17 +0200

    init

---
+++ DemoGoogleMaps/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DemoGoogleMaps
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        static string GeneratedHTML = "";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //Load Data method is called to populate the default.html string which we are rendering in
+            //our WebView.
+            LoadData();
+            //Setting the HTML content in the WebView control.
+            MapWebView.NavigateToString(GeneratedHTML);
+        }
+
+        private async void LoadData()
+        {
+            try
+            {
+                await loadJsonLocalnew("default.html", "");
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async static Task loadJsonLocalnew(string url, object ClassName)
+        {
+            try
+            {
+                //*******************************************************
+                //To pick up file from local folder in window store app
+                //*******************************************************
+                StorageFolder folder = await Package.Current.InstalledLocation.GetFolderAsync("Map");
+                StorageFile file = await folder.GetFileAsync(url);
+                Stream stream = await file.OpenStreamForReadAsync();
+                StreamReader reader = new StreamReader(stream);
+                String html = reader.ReadToEnd();
+                GeneratedHTML = html.ToString();
+                //*******************************************************
+                //*******************************************************
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ DemoGoogleMaps/Map/mymap.js
@@ -0,0 +1,64 @@
+﻿var map;
+var earthquakeResults;
+
+//Intialization of the google map API is done here
+function initialize() {
+    //here we are getting the Element ID of the mapwindow of mymap.html to render google map there.
+    map = new google.maps.Map(document.getElementById('mapwindow'), {
+        zoom: 3,
+        center: new google.maps.LatLng(40, -187.3),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+
+    addMarkers();
+}
+
+//this function is extracting data from week.js for the use in the Map
+eqfeed_callback = function (results) {
+    earthquakeResults = results;
+}
+
+//below function is used to add pins to the specific location on the google map.
+var marker;
+popUps = new Array();
+positions = new Array();
+function addMarkers() {
+    for (var i = 0; i < earthquakeResults.features.length; i++) {
+
+        var earthquake = earthquakeResults.features[i];
+        var coordinates = earthquake.geometry.coordinates;
+        var latiLong = new google.maps.LatLng(coordinates[1], coordinates[0]);
+        var place = earthquake.properties.place;
+        marker = new google.maps.Marker({
+            position: latiLong,
+            map: map,
+        });
+
+        ///**************************************************************************************////
+        ///This part is done to show the information popup when users click on the particular pin
+        ///if not needed commenting this code will not affect the map
+        ///**************************************************************************************////
+        positions[i] = marker;
+        popUps[i] = '<div id = "content">' +
+        '<div>' +
+        '</div>' +
+        '<p>' + place + '</h1>' +
+        '</div>';
+        //Here the addListener is being invoked which is in turn invoking particular info window.
+        addListener(i);
+        ///**************************************************************************************////
+        ///**************************************************************************************////
+    }
+}
+
+var infowindows;
+var addListener = function (i) {
+    google.maps.event.addListener(marker, 'click', function () {
+        if (infowindows) infowindows.close();
+        infowindows = new google.maps.InfoWindow({
+            content: popUps[i]
+        });
+        infowindows.open(map, positions[i]);
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
